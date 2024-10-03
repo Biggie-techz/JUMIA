9fd57edb65f4a2362d8de004fdd08d0843c61c4a
Adewole Ayonitemi <adewoleayonitemi@gmail.com>
2024-10-03 18:55:57 +0100

    First deploy

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPzH_KoY2sotcvyFu773nNr0WiFhzRZnM",
+  authDomain: "jumia-950fe.firebaseapp.com",
+  projectId: "jumia-950fe",
+  storageBucket: "jumia-950fe.appspot.com",
+  messagingSenderId: "556601518531",
+  appId: "1:556601518531:web:00a72d6a48c52d58ecf576",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
